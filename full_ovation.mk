a340ad25724d085782cf99b1ccd4f68c4c7a361b
Hashcode <hashcode0f@gmail.com>
2013-08-15 16:11:16 -0700

    reverse build product order to fix prop set order
    Change-Id: Iaa453088252502536df0f25c4b9c3b89af3df221

--- full_ovation.mk
+++ full_ovation.mk
@@ -20,11 +20,11 @@
 # product configuration (apps).
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-# This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/bn/ovation/device.mk)
+# This is where we'd set a backup provider if we had one
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_ovation
