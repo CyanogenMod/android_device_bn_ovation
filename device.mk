004a1e1b03d9b968a3fa17bf9c59ab11e98b6c22
Robert Yang <rhyang@rhysw.com>
2014-10-26 10:44:10 -0700

    Revert "Nook camera stub (2/2)"
    This reverts commit c6d65924ef62941d591d56e0b8a42f80dca38376.  Skype started
    choking on the camera stub, an arguably more important consideration
    than the ability to view photospheres.
    
    Change-Id: I38c90d7eff5067541c97083217c4fc6cffe9dd23

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@
 DEVICE_FOLDER := device/bn/ovation
 TARGET_BOOTLOADER_BOARD_NAME := ovation
 
-# use stub camera from device/bn/common.
-BN_CAMERA_STUB := true
-
 $(call inherit-product, device/bn/common/common.mk)
 
 # Device overlay
